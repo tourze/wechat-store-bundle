c9c0c6c6ff6b6cd9313b9e4092a2b7e85a764423
github-ci <you@example.com>
2025-11-03 14:28:07 +0800

    refactor(user-agreement-bundle): 解耦 BizUserBundle，使用 user-service-contracts 接口

--- src/WechatStoreBundle.php
+++ src/WechatStoreBundle.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\RoutingAutoLoaderBundle\RoutingAutoLoaderBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class WechatStoreBundle extends Bundle implements BundleDependencyInterface
 {
@@ -14,6 +15,7 @@ class WechatStoreBundle extends Bundle implements BundleDependencyInterface
         return [
             DoctrineBundle::class => ['all' => true],
             RoutingAutoLoaderBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
